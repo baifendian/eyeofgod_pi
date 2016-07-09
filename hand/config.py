73ba1149369e102b44c220f7593519e546dde5cd
weisong.hu <weisong.hu@baifendian.com>
2016-07-09 09:29:00 +0800

    增加服务器配置

--- hand/config.py
+++ hand/config.py
@@ -1,30 +1,45 @@
 
 
+
+
+SERVER_HOST = "192.168.1.140"
+SERVER_PORT = "9000"
+SERVER_PATH = "/sensor/postdata"
+SERVER_URL  = "http://" + SERVER_HOST + ":" + SERVER_PORT + SERVER_PATH
+
+
 SENSOR_MAP = {
     "123456" : {
         "type"  : 1, 
         "module": "sensorDistance",
         "clazz" : "SensorDistance", 
-        "pins"  : {
+        "args"  : {
             "ECHO" : 11, 
-            "TRIG" : 12
+            "TRIG" : 12,
+            "LIMIT" : 20
         }
     },
     "adsfgh" : {
         "type"  : 1, 
         "module": "sensorDistance",
         "clazz" : "SensorDistance", 
-        "pins"  : {
+        "args"  : {
             "ECHO" : 11, 
-            "TRIG" : 12
+            "TRIG" : 12,
+            "LIMIT" : 20
         }
     },
     "234567" : {
         "type"  : 2,
         "module": "sensorMotion",
         "clazz" : "SensorMotion",
-        "pins"  : {
+        "args"  : {
             "PINN" : 15
         }
     }
 }
+
+
+
+if __name__ == "__main__" :
+    print SERVER_URL
