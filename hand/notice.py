461e7c9ff12cc4daee3c5c8caffa092270851fd8
muxianliang <mxl_1_68@163.com>
2016-07-09 16:11:36 +0800

    merge from xianliang.mu

--- hand/notice.py
+++ hand/notice.py
@@ -1,19 +1,25 @@
-
-
-
-import config
+import urllib
 import urllib2
 
+#message: { 'id':$id , 'distance':$distance }
 
+#the tool class to dispatch message to  server
 class Notice :
 
-    def __init__(self) :
-        conn = urllib2.open()
+    #def __init__(self) :
+        # need do nothine
+    #    pass
 
 
-    def notify(self, message) :
-        pass
-
-
-
-
+    @staticmethod
+    def notify(message):
+        url = "http://192.168.1.140:9000/sensor/postdata"
+        ms_encode = urllib.urlencode(message)
+        req = urllib2.Request(url,ms_encode)
+        sock = urllib2.urlopen(req)
+        html = sock.read()
+        code = sock.getcode()
+        sock.close()
+        print html
+        print code
+        return code
