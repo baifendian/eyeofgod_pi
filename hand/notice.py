0934febef04c1ea4b99080d1cdcf73ee588c768d
muxianliang <mxl_1_68@163.com>
2016-07-09 20:14:31 +0800

    update notice

--- hand/notice.py
+++ hand/notice.py
@@ -11,14 +11,14 @@ import urllib2
 #the tool class to dispatch message to  server
 class Notice :
 
-    def __init__(self) :
-        self.url = config.SERVER_URL
+    #def __init__(self) :
+    #    self.url = config.SERVER_URL
 
 
     @staticmethod
     def notify(message):
         ms_encode = urllib.urlencode(message)
-        req = urllib2.Request(self.url, ms_encode)
+        req = urllib2.Request(config.SERVER_URL, ms_encode)
         sock = urllib2.urlopen(req)
         html = sock.read()
         code = sock.getcode()
