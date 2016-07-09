410db3647794b00a282d0b9da2c2372ab8cf8ef0
weisong.hu <weisong.hu@baifendian.com>
2016-07-09 09:33:54 +0800

    url从配置文件中读取

--- hand/notice.py
+++ hand/notice.py
@@ -1,21 +1,24 @@
+
+
+import config
 import urllib
 import urllib2
 
+
 #message: { 'id':$id , 'distance':$distance }
+
 
 #the tool class to dispatch message to  server
 class Notice :
 
-    #def __init__(self) :
-        # need do nothine
-    #    pass
+    def __init__(self) :
+        self.url = config.SERVER_URL
 
 
     @staticmethod
     def notify(message):
-        url = "http://192.168.1.140:9000/sensor/postdata"
         ms_encode = urllib.urlencode(message)
-        req = urllib2.Request(url,ms_encode)
+        req = urllib2.Request(self.url, ms_encode)
         sock = urllib2.urlopen(req)
         html = sock.read()
         code = sock.getcode()
