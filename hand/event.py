f3bf8949801ac6b1a174ed195bd4736b0d6f32c8
muxianliang <mxl_1_68@163.com>
2016-07-09 16:12:36 +0800

    merge from xianliang.mu

--- hand/event.py
+++ hand/event.py
@@ -1,13 +1,19 @@
-
-
-
-
 class Event :
 
-    def __init__(self, gid, type, location, number) :
-        self.gid         = gid
-        self.type        = type
-        self.location    = location
-        self.number      = number
-
-
+    def __init__(self, sensor_type,sensor_id, pi_mark, timestamp,state) :
+        self.sensor_type  = sensor_type 
+        self.sensor_id    = sensor_id
+        self.pi_mark      = pi_mark
+        self.timestamp    = timestamp
+        self.state        = state
+        self.mark         = pi_mark + '_' + sensor_id
+    
+    def buildMessage(self)
+        message = {}
+        message['sensor_type'] = self.sensor_type
+        message['sensor_id'] = self.sensor_id
+        message['pi_mark'] = self.pi_mark
+        message['timestamp'] = self.timestamp
+        message['state'] = self.state
+        message['mark'] = self.mark
+        return message
