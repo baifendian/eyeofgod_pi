7e3ab084c89a326d863ea7a6e90fa48d6c3f5ceb
weisong.hu <weisong.hu@baifendian.com>
2016-07-09 09:32:45 +0800

    实现移动传感器主体逻辑

--- hand/sensorMotion.py
+++ hand/sensorMotion.py
@@ -1,17 +1,27 @@
 
 
 import sensor
-
+import RPi.GPIO as GPIO
 
 
 class SensorMotion(sensor.Sensor) :
 
-    def __init__(self) :
-        pass
+    def __init__(self, args) :
+        self.PINN = args["PINN"]
+        GPIO.setmode(GPIO.BCD)
+        GPIO.setup(PINN, GPIO.IN)
 
     def detect(self) :
-        print "motion"
-        pass
+        if GPIO.input(self.PINN) :
+            print "True"
+            return True
+        else :
+            return Flase
 
+
+if __name__ == "__main__" :
+    args = {"PINN" : 18}
+    sensor = SensorMotion(args)
+    sensor.detect()
 
 
